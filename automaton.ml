522d8d81da864a2650e8be0caae8c3e6d0b251f7
Do Quoc Anh <thiensuhoabinh_quocanh_91@yahoo.com>
2013-10-12 20:09:26 +0700

    Checking Lt condition
    Checking Lt condition with average automaton (calculate average value
    for input satisfy lying in interval (3,10) . Print out error input

--- automaton.ml
+++ automaton.ml
@@ -1,72 +1,68 @@
 	(* Set *)
+	
 	module Set = 
 	(struct
-	type 't set = 't list
+	
+		type 't set = 't list
+		
+		type 't t = 't set
 
-	type 't t = 't set
+		let (add: 't -> 't set -> 't set) = 
+			fun e s -> if List.mem e s then s else e::s 
 
-	let (add: 't -> 't set -> 't set) = 
-	fun e s -> if List.mem e s then s else e::s 
+		let (union: 't set -> 't set -> 't set) = 
+			fun s1 s2 -> List.sort Pervasives.compare (List.fold_right add s1 (List.fold_right add s2 []))
 
-	let (union: 't set -> 't set -> 't set) = 
-	fun s1 s2 -> List.sort Pervasives.compare (List.fold_right add s1 (List.fold_right add s2 []))
-
-	let (subseteq: 't set -> 't set -> bool) =
-	fun s1 s2 -> List.for_all (fun e1 -> List.mem e1 s2) s1
+		let (subseteq: 't set -> 't set -> bool) =
+			fun s1 s2 -> List.for_all (fun e1 -> List.mem e1 s2) s1
 
 	end)
 
 
-	(* automatonOMATON *)
-
+	(* AUTOMATON *)
 
 	type 'elt set = 'elt Set.t
-
-
 	(* symbol *)
 	type symbol = string
 	let epsilon = ""
 	type alphabet = symbol set
-	type input = int * int
-
+	
+	type type_inp = int
+	type type_out = int
+	
+	type input = { 
+			sym: symbol; 
+			inp: type_inp ;
+			out: type_out;
+	}						
+	
 	(* transition *)
-
-
 	type 'state transition = 'state * symbol * 'state
-	
-	type 'state transitions = ('state transition) set
-	
-	(*	type input = symbol*int *)
-	
 	 
-	(* automatonomaton *)
+	(* automaton *)
 
-	type 'state automatonomaton = 
-	{ location: 'state set
-	; alphabet: symbol set
-	; initial: 'state set
-	; accepting: 'state set
-	; transitions: ('state transition) list
-	(*	; Input : input list *)
-
+	type 'state automaton = 
+	{ 	location: 'state set; 
+		alphabet: symbol set; 
+		initial: 'state set; 
+		accepting: 'state set; 
+		transitions: ('state transition) list;
+		inputs : input list;
 	}
 	
-
 	type 'state selection = 
-	| From of 'state list
-	| To   of 'state list
-	| On   of symbol list
-	| On_all_but of symbol list
+		| From of 'state list
+		| To   of 'state list
+		| On   of symbol list
+		| On_all_but of symbol list
 
 
 	let (get_transitions: 'state selection -> ('state transition) list -> ('state transition) list) = 
-	fun selection transitions -> 
-	match selection with
-	| From states -> List.filter (fun (q,_,_) -> List.mem q states) transitions 
-	| To states   -> List.filter (fun (_,_,q) -> List.mem q states) transitions 
-	| On symbols         -> List.filter (fun (_,s,_) -> List.mem s symbols) transitions 
-	| On_all_but symbols -> List.filter (fun (_,s,_) -> not (List.mem s symbols)) transitions 
-
+		fun selection transitions -> match selection with
+			| From states -> List.filter (fun (q,_,_) -> List.mem q states) transitions 
+			| To states   -> List.filter (fun (_,_,q) -> List.mem q states) transitions 
+			| On symbols         -> List.filter (fun (_,s,_) -> List.mem s symbols) transitions 
+			| On_all_but symbols -> List.filter (fun (_,s,_) -> not (List.mem s symbols)) transitions 
 
 	let (targets_of: ('state transition) list -> 'state set) =
 	fun transitions -> Set.union [] (List.map (fun (_,_,q) -> q) transitions)
@@ -77,55 +73,23 @@
 	let (states_of: ('state transition) list -> 'state set) = 
 	fun transitions -> Set.union (sources_of transitions) (targets_of transitions) 
 
-	let (number_of_states: 'state automatonomaton -> int) = 
-	fun automaton -> List.length (states_of automaton.transitions)
-
-	let (targets: 'state automatonomaton -> 'state set -> symbol -> 'state set) = 
+	let (targets: 'state automaton -> 'state set -> symbol -> 'state set) = 
 	fun automaton states symbol -> targets_of (get_transitions (On [symbol]) (get_transitions (From states) automaton.transitions))  
 
-	(*
-	let rec is_accepted automaton word current_state = match word with
-	|head::tail::[] -> 	if Set.subseteq (automaton.accepting) (targets automaton current_state head) then 
-							if (int_of_string(tail) < 5) 
-								then print_string "Recognized"
-							else 
-								print_string "Not";
-	|head::tail -> 	match tail with head1::tail1 -> if (int_of_string(head1) < 5) then is_accepted automaton tail1 (targets automaton current_state head)
-													else print_string "Not accept"
-	*)
-	(* tuple *)
 	
-	let x = ref 0.
-	let sum = ref 0.
-	
-	let rec is_accepted automaton tuples_list current_state = match tuples_list with
-	|[] -> print_string "Not_found"
-	|(character, value_lt, value_ls)::[] -> if (value_lt < 10.) then
-												begin
-													x := !x +. 1.;
-													sum:= !sum +. value_lt;
-													if (value_ls = !sum/. !x) then												
-														if Set.subseteq (automaton.accepting) (targets automaton current_state character) then  
-															print_string "Recognize!"
-														else
-															print_string "Not Recognized!"
-													else
-														print_string "Not accept because not satisfied LS!"
-												end
-											else 
-												print_string "Not accept because not satisfied LT!";
-	|(character, value_lt, value_ls)::tl -> if (value_lt < 10.) then 
-												begin
-													x := !x +. 1.;
-													sum:= !sum +. value_lt;
-													if (value_ls = !sum/. !x) then
-														is_accepted automaton tl (targets automaton current_state character)
-													else
-														print_string "Not accept because not satisfied LS!"
-												end
-											else 
-												print_string "Not accept because not satisfied LT!"
-
-
-	
-	
+	(* Checking Lt condition *)	
+	let rec check_lt automaton inputs current_state sum count = match inputs with
+		| e1::[] -> if e1.inp < 10 && e1.inp > 3 then begin 
+															if ((sum + e1.inp)/count == e1.out) then if Set.subseteq (automaton.accepting) (targets automaton current_state e1.sym ) then  
+																								print_string "Recognize!"
+																								else print_string "Not Recognized!"
+															else  print_string ("Not satisfied Lt at input [" ^e1.sym^ ","^ string_of_int(e1.inp) ^"," ^ string_of_int(e1.out)^"]")
+													end
+		| e1::tail -> if (e1.inp < 10 && e1.inp > 3)  then	begin 
+																if ((sum + e1.inp)/count == e1.out) then begin 
+																						check_lt automaton tail (targets automaton current_state e1.sym) (sum+e1.inp) (count+1)														
+																					end
+																else  print_string ("Not satisfied Lt at input [" ^e1.sym^ ","^ string_of_int(e1.inp) ^"," ^ string_of_int(e1.out)^"]")
+															end
+						else  print_string ("Not satisfied Lt at input [" ^e1.sym^ ","^ string_of_int(e1.inp) ^"," ^ string_of_int(e1.out)^"]")
+													
--- test.ml
+++ test.ml
@@ -1,13 +1,37 @@
-let odd = {
-	location = [0;1];
+(* Correct input satisfy Lt condition and input fit output *)
+let average1 = {
+	location = [0];
 	alphabet = ["a"];
 	initial = [0];
-	accepting = [1];
-	transitions = [(0,"a",1);(1,"a",0)]
+	accepting = [0];
+	transitions = [(0,"a",0)];
+	inputs = [{sym = "a"; inp = 4; out = 4}; {sym = "a"; inp = 6; out = 5};{sym = "a"; inp = 8 ; out = 6}];
+};;
+check_lt average1 average1.inputs average1.initial 0 1;;
+
+(* Incorrect input satisfy Lt condition but input and output don't fit *)
+let average2 = {
+	location = [0];
+	alphabet = ["a"];
+	initial = [0];
+	accepting = [0];
+	transitions = [(0,"a",0)];
+	inputs = [{sym = "a"; inp = 4; out = 4}; {sym = "a"; inp = 6; out = 5};{sym = "a"; inp = 8 ; out = 7}];
 };;
 
+check_lt average2 average2.inputs average2.initial 0 1;;
 
-(*is_accepted odd [("a";"2";"a";"4";"a";"3"] odd.initial;;*)
-is_accepted odd [("a", 4., 4.); ("a", 2., 3.); ("a", 3., 3.); ("a", 7., 4.);("a", 2., 3.7)] odd.initial
+(* Incorrect input not satisfy Lt condition *)
+
+let average3 = {
+	location = [0];
+	alphabet = ["a"];
+	initial = [0];
+	accepting = [0];
+	transitions = [(0,"a",0)];
+	inputs = [{sym = "a"; inp = 4; out = 4}; {sym = "a"; inp = 2; out = 5};{sym = "a"; inp = 8 ; out = 7}];
+};;
+
+check_lt average3 average3.inputs average3.initial 0 1;;
 
 
